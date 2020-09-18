26a8885fcef3db313cb751f9cfd6690775b067d5
Peter Nordman <peter.m.nordman@gmail.com>
2020-09-17 21:44:01 -0400

    init commit, added basic homepage component and sass styles

---
+++ src/homepage.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div classname='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className="title">SOFT</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className="title">AQUILINE</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className="title">COLOURFUL</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className="title">REGAL</h1>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className="content">
+                    <h1 className="title">WHIMSICAL</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
