c25d64ab841f8be8f7007fbaf29e40b2463442ec
Peter Nordman <peter.m.nordman@gmail.com>
2020-09-21 20:25:43 -0400

    Add FormInput component and styles, update Readme

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route, Switch, Link, withRouter } from "react-router-dom";
+import { Route, Switch, withRouter } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/header/header.component";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App({ location }) {
   return (
@@ -14,6 +15,7 @@ function App({ location }) {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/components/form-input/form-input.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import './form-input.styles.scss';
+
+const FormInput = ({ handleChange, label, ...otherProps }) => {
+  // We're going to bubble up any change on the component via handleChange
+
+  return (
+    <div className="group">
+      <input
+        className="form-input"
+        onChange={(handleChange)}
+        {...otherProps}
+      />
+      {label && (
+        <label
+          classname={`${otherProps.value.length ? "shrink" : ""} form-input-label`}
+        >
+          {label}
+        </label>
+      )}
+    </div>
+  );
+};
+
+export default FormInput;
