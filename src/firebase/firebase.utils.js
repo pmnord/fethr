7d796b5501345b6a05ec9235c2af125f7a410fab
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-18 08:48:49 -0500

    Add custom-button component. Add config for firebase.

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: process.env.API_KEY,
+  authDomain: "fethr-db.firebaseapp.com",
+  databaseURL: "https://fethr-db.firebaseio.com",
+  projectId: "fethr-db",
+  storageBucket: "fethr-db.appspot.com",
+  messagingSenderId: "351150444097",
+  appId: "1:351150444097:web:e614824555aab72920c0c0",
+  measurementId: "G-3J351XYX9W"
+};
