5ff64e1b5c749499d248935fee82d70ee29127a9
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-20 16:58:26 -0500

    Add custom-button to collection-item and nested styling in collection-item.styles

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./collection-item.styles.scss";
 
+import CustomButton from "../custom-button/custom-button.component";
+
 const CollectionItem = ({ id, name, imageUrl, price }) => {
   return (
     <div className="collection-item">
@@ -9,12 +11,12 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
-      >
+      ></div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">${price}</span>
       </div>
-        <div className="collection-footer">
-          <span className="name">{name}</span>
-          <span className="price">${price}</span>
-        </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
