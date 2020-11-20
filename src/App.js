4e340df3a740431942cba2d593d4e0cc30810c23
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-20 15:05:54 -0500

    Update App to use Redirect component to push users to root when accessing disallowed routes. Add types to User reducer and actions.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import React from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { compose } from "redux";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
+
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
   constructor(props) {
@@ -49,8 +49,18 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
-          <Route path="/fethr" render={({ history }) => history.push("/")} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
+          <Route path="/fethr" render={() => <Redirect to="/" />} />
           {/* Github hosting is at the /fethr route so the above line autopushes to root */}
         </Switch>
       </div>
@@ -58,8 +68,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default compose(withRouter, connect(null, mapDispatchToProps))(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
