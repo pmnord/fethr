26ffc7a7f1a55dc701bd0239d0e67821f021213d
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-23 23:03:58 -0500

    Add shop.reducer and shop.selectors. Move shop data into redux Store. Refactor shop page to retrieve data from Store.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from "redux-persist/lib/storage"; // Tells redux-persist to use l
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer.js";
 import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
 const persistConfig = {
   key: "root", // Where in reducer to start reducing from
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
   directory: directoryReducer,
+  shop: shopReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
