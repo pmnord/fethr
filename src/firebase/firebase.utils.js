8484e03302b5fb53a7b749f5d25fbc6b6cfaaddf
Peter Nordman <peter.m.nordman@gmail.com>
2020-11-26 11:48:09 -0500

    Add heroku-js-runtime-env package to enable runtime environmental variables in heroku cra buildpack

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
+import runtimeEnv from "@mars/heroku-js-runtime-env";
+
+const env = runtimeEnv();
 
 const config = {
-  apiKey: process.env.REACT_APP_firebase_apiKey,
+  apiKey: env.REACT_APP_firebase_apiKey,
   authDomain: "fethr-db.firebaseapp.com",
   databaseURL: "https://fethr-db.firebaseio.com",
   projectId: "fethr-db",
   storageBucket: "fethr-db.appspot.com",
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId,
+  messagingSenderId: env.REACT_APP_messagingSenderId,
+  appId: env.REACT_APP_appId,
+  measurementId: env.REACT_APP_measurementId,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
